79119575f1698fa7389fbb2aada20c1b7eff9c8a
Rexben <rexben.rb@gmail.com>
2019-05-06 04:33:25 +0100

    Updates

---
+++ precache-manifest.f5ff853e13f66f1835d9325f7de41679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b868de99159d1c261ff2",
+    "url": "/reporting-react/static/css/main.526fa35d.chunk.css"
+  },
+  {
+    "revision": "b868de99159d1c261ff2",
+    "url": "/reporting-react/static/js/main.49b05f28.chunk.js"
+  },
+  {
+    "revision": "72ec91de8978d8a3f212",
+    "url": "/reporting-react/static/js/runtime~main.9354857b.js"
+  },
+  {
+    "revision": "9deb31aaab2205864acd",
+    "url": "/reporting-react/static/js/2.4935a8b3.chunk.js"
+  },
+  {
+    "revision": "d37f14cc7e04e536937d0e84d2f2b11e",
+    "url": "/reporting-react/static/media/home.d37f14cc.jpg"
+  },
+  {
+    "revision": "20385b0db47a00dcdfbf1e69467d0647",
+    "url": "/reporting-react/index.html"
+  }
+];
